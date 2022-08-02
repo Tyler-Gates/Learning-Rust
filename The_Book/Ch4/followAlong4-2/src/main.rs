7273767b12f8fe2213fc7acb36d67a4859219665
Tyler-Gates <65058265+Tyler-Gates@users.noreply.github.com>
2022-08-01 23:03:19 -0500

    Chapter 4 complete!

---
+++ The_Book/Ch4/followAlong4-2/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("hello");
+
+    change(&mut s);
+
+
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn dangle() -> String { // dangle returns a reference to a String
+
+    let s = String::from("hello"); // s is a new String
+
+    s // we return a reference to the String, s
+} // Here, s goes out of scope, and is dropped. Its memory goes away.
+  // Danger!

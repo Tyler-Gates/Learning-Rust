254cfd017634ebd8d2d4af5ab7d2cd1b1a1ecdff
Tyler-Gates <65058265+Tyler-Gates@users.noreply.github.com>
2022-08-20 14:24:11 -0500

    13.1 complete

--- The_Book/Ch13/ch13_1/src/main.rs
+++ The_Book/Ch13/ch13_1/src/main.rs
@@ -1,3 +1,6 @@
+use std::thread;
+use std::time::Duration;
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum ShirtColor {
     Red,
@@ -33,7 +36,63 @@ impl Inventory {
     }
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
+    //its titled expensive because we don't need to be verbose by adding type annotations, this could be done with |num: u32| only
+    let expensive_closure = |num: u32| -> u32 {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    //shows how to simplify syntax from functio to closures
+    fn  add_one_v1   (x: u32) -> u32 { x + 1 } //function
+    let add_one_v2 = |x: u32| -> u32 { x + 1 }; //fully annotated closure
+    let add_one_v3 = |x|             { x + 1 }; //remove type annotations from closure definition
+    let add_one_v4 = |x|               x + 1  ; //remove the brackets
+    let y = add_one_v3(3);//Nothing within the closure body lets compiler deduce the type, so we need to call the closure variables otherwise the compiler complains with:
+    let u = add_one_v4(3);//^ consider giving this closure parameter a type
+
+    //compiler will infer ONE concrete type only! After you call it once with a string, it will always be required that x is a string, that is why n is commented out
+    let example_closure = |x| x;
+    let s = example_closure(String::from("hello"));
+    //let n = example_closure(5);
+
+    //closures capture values in three ways, take ownership, borrow immutably or mutably.
+    //immutably
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+    let only_borrows = || println!("From closure: {:?}", list);
+//
+    println!("Before calling closure: {:?}", list);
+    only_borrows(); //grabs immutable reference
+    println!("After calling closure: {:?}", list);
+
+    //mutably
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+    let mut borrows_mutably = || list.push(7);//closure definition
+//Between the closure definition and the closure call, an immutable borrow to print isn’t allowed because no other borrows are allowed when there’s a mutable borrow. 
+    borrows_mutably();//closure call to grab mut reference
+    println!("After calling closure: {:?}", list);
+
+
+
+
+    let mut list = [
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
+
+    list.sort_by_key(|r| r.width);//takes a FnMut closure Called multiple times, once for each item in the slice
+    println!("{:#?}", list);
+
     let store = Inventory {
         shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
     };
@@ -52,3 +111,4 @@ fn main() {
         user_pref2, giveaway2
     );
 }
+

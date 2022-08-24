b4e867c385e7ef2c6dc7c6972633169420a7aa2c
Tyler-Gates <65058265+Tyler-Gates@users.noreply.github.com>
2022-08-23 23:56:32 -0500

    Ch15.1: Using Box<t> to Point to Data on the Heap

---
+++ The_Book/Ch15/ch15_1/src/main.rs
@@ -0,0 +1,24 @@
+use crate::List::{Cons, Nil};
+
+// compiler will complain here because List is a recursive type that has theoritically infinite size
+// this is where the Box<T> comes in to play
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn main() {
+    let b = Box::new(5);
+    println!("b = {}", b);
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+
+}

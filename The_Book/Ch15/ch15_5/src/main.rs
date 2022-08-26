f55978c84236ff6d4e11081c19da0718fedff67a
Tyler-Gates <65058265+Tyler-Gates@users.noreply.github.com>
2022-08-25 23:50:21 -0500

    15.5: Working with RefCell<T> & Interior Mutability

---
+++ The_Book/Ch15/ch15_5/src/main.rs
@@ -0,0 +1,27 @@
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+
+//The RefCell<T> type is useful when you’re sure your code follows the borrowing rules but the compiler is unable to understand and guarantee that.
+//only for use in single threaded scenarios
+fn main() {
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+}

29f8326f0af0b63829bb9cb761e6676f5afa3501
Tyler-Gates <65058265+Tyler-Gates@users.noreply.github.com>
2022-08-28 20:11:37 -0500

    Ch16.3: Shared-State Concurrency

---
+++ The_Book/Ch16/ch16_3/src/main.rs
@@ -0,0 +1,37 @@
+use std::sync::{Mutex, Arc};
+use std::thread;
+
+//mutex = mutual exclusion = only allow 1 thread to access some data at any given time
+//mutex are guards of the data it holds
+fn main() {
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
+
+    //arc = atomic reference counting
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
+
+//NOTES: for updating primitive types, the std::sync::atomic module allows for this without mutexes

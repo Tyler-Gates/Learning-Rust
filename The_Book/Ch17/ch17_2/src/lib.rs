a66569c9db911cdc5cec20b2c89701288fc94465
Tyler-Gates <65058265+Tyler-Gates@users.noreply.github.com>
2022-08-30 23:31:42 -0500

    Ch17.3:Implementing OOPD

--- The_Book/Ch17/ch17_2/src/lib.rs
+++ The_Book/Ch17/ch17_2/src/lib.rs
@@ -4,6 +4,8 @@ pub trait Draw {
 
 pub struct Screen {
     //dyn Draw is a stand in for any type inside Box that implements the draw trait
+    //doing things this way utilizes dynamic dispatch, a determination made at runtime to see which method to call
+    //this incurs penalties to runtime cost, and some optimizations
     pub components: Vec<Box<dyn Draw>>,
 }
 

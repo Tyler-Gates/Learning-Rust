120dc7ac78216f14e29be6d3402cde59bd6fd7f9
Tyler-Gates <65058265+Tyler-Gates@users.noreply.github.com>
2022-10-07 20:36:58 -0500

    Conway's game of life base complete

--- GameOfLife/src/board.rs
+++ GameOfLife/src/board.rs
@@ -8,6 +8,10 @@ pub struct Board {
 
 
 impl Board {
+    pub fn custom_instantiation(board: Vec<Vec<bool>>) -> Board {
+        Board { board }
+    }
+
     pub fn random_state_instantiation(width: u32, height: u32) -> Board {
         let mut board: Vec<Vec<bool>> = Vec::new();
         for i in 0..height {
@@ -19,10 +23,201 @@ impl Board {
         }
         Board { board }
     }
+
+    pub fn dead_state_instantiation(width: u32, height: u32) -> Board {
+        let mut board: Vec<Vec<bool>> = Vec::new();
+        for i in 0..height {
+            let mut temp = Vec::new();
+            for j in 0..width {
+                temp.push(false);
+            }
+            board.push(temp);
+        }
+        Board { board }
+    }
+
+    pub fn next_board_state(mut self) -> Self {
+        let mut newboard: Vec<Vec<bool>> = Vec::new();
+        let mut top = false;
+        let mut left = false;
+        let mut right = false;
+        let mut bottom = false;
+
+        for i in 0..self.board.len() {
+            let mut temp: Vec<bool> = Vec::new();
+            for j in 0..self.board[0].len() {
+                let mut count = 0;
+                left = j == 0;
+                top = i == 0;
+                right = j == self.board[0].len() - 1;
+                bottom = i == self.board.len() - 1;
+                if top && left {
+                    if self.board[i][j+1] {
+                        count = count + 1;
+                    }
+                    if self.board[i+1][j+1] {
+                        count = count + 1;
+                    }
+                    if self.board[i+1][j] {
+                        count = count + 1;
+                    }
+                }
+                else if top && right {
+                    if self.board[i][j-1] {
+                        count = count + 1;
+                    }
+                    if self.board[i+1][j-1] {
+                        count = count + 1;
+                    }
+                    if self.board[i+1][j] {
+                        count = count + 1;
+                    }
+                }
+                else if bottom && right {
+                    if self.board[i][j-1] {
+                        count = count + 1;
+                    }
+                    if self.board[i-1][j-1] {
+                        count = count + 1;
+                    }
+                    if self.board[i-1][j] {
+                        count = count + 1;
+                    }
+                }
+                else if bottom && left {
+                    if self.board[i][j+1] {
+                        count = count + 1;
+                    }
+                    if self.board[i-1][j+1] {
+                        count = count + 1;
+                    }
+                    if self.board[i-1][j] {
+                        count = count + 1;
+                    }
+                }
+                else if top {
+                    if self.board[i+1][j] {
+                        count = count + 1;
+                    }
+                    if self.board[i+1][j+1] {
+                        count = count + 1;
+                    }
+                    if self.board[i+1][j-1] {
+                        count = count + 1;
+                    }
+                    if self.board[i][j+1] {
+                        count = count + 1;
+                    }
+                    if self.board[i][j-1] {
+                        count = count + 1;
+                    }
+                }
+                else if bottom {
+                    if self.board[i-1][j] {
+                        count = count + 1;
+                    }
+                    if self.board[i-1][j+1] {
+                        count = count + 1;
+                    }
+                    if self.board[i-1][j-1] {
+                        count = count + 1;
+                    }
+                    if self.board[i][j+1] {
+                        count = count + 1;
+                    }
+                    if self.board[i][j-1] {
+                        count = count + 1;
+                    }
+                }
+                else if right {
+                    if self.board[i-1][j] {
+                        count = count + 1;
+                    }
+                    if self.board[i-1][j-1] {
+                        count = count + 1;
+                    }
+                    if self.board[i][j-1] {
+                        count = count + 1;
+                    }
+                    if self.board[i+1][j-1] {
+                        count = count + 1;
+                    }
+                    if self.board[i+1][j] {
+                        count = count + 1;
+                    }
+                }
+                else if left {
+                    if self.board[i-1][j] {
+                        count = count + 1;
+                    }
+                    if self.board[i-1][j+1] {
+                        count = count + 1;
+                    }
+                    if self.board[i][j+1] {
+                        count = count + 1;
+                    }
+                    if self.board[i+1][j+1] {
+                        count = count + 1;
+                    }
+                    if self.board[i+1][j] {
+                        count = count + 1;
+                    }
+                }
+                else {
+                    if self.board[i-1][j] {
+                        count = count + 1;
+                    }
+                    if self.board[i-1][j-1] {
+                        count = count + 1;
+                    }
+                    if self.board[i][j-1] {
+                        count = count + 1;
+                    }
+                    if self.board[i+1][j-1] {
+                        count = count + 1;
+                    }
+                    if self.board[i+1][j] {
+                        count = count + 1;
+                    }
+                    if self.board[i+1][j+1] {
+                        count = count + 1;
+                    }
+                    if self.board[i][j+1] {
+                        count = count + 1;
+                    }
+                    if self.board[i-1][j+1] {
+                        count = count + 1;
+                    }
+                }
+                if count <= 1 {
+                    temp.push(false);
+                }
+                else if count == 2 {
+                    if self.board[i][j] == true {
+                        temp.push(true);
+                    }
+                    else {
+                        temp.push(false);
+                    }
+                }
+                else if count == 3 {
+                    temp.push(true);
+                }
+                else if count > 3 {
+                    temp.push(false);
+                }
+            }
+            newboard.push(temp);
+        }
+        self.board = newboard;
+        self
+    }
+
 }
 
 impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}[2J", 27 as char);
         for i in 0..self.board.len() {
             let mut text = String::new();
             for j in 0..self.board[0].len() {
@@ -38,4 +233,92 @@ impl fmt::Display for Board {
         }
         Ok(())
     }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::Board;
+
+    #[test]
+    pub fn test_next_on_dead() {
+        let mut deadboard = vec![vec![false,false,false],
+                                 vec![false,false,false],
+                                 vec![false,false,false]];
+        let mut game = Board::custom_instantiation(deadboard.clone());
+        game = game.next_board_state();
+        assert_eq!(game.board, deadboard);
+    }
+
+    #[test]
+    pub fn test_next_on_one_alive() {
+        let mut oneboard =  vec![vec![false,false,false],
+                                 vec![false,true,false],
+                                 vec![false,false,false]];
+
+        let mut deadboard = vec![vec![false,false,false],
+                                 vec![false,false,false],
+                                 vec![false,false,false]];
+        let mut game = Board::custom_instantiation(oneboard);
+        game = game.next_board_state();
+        assert_eq!(game.board, deadboard);                    
+    }
+
+    #[test]
+    pub fn test_next_on_two_alive() {
+        let mut twoboard =  vec![vec![false,false,false],
+                                 vec![false,true,true],
+                                 vec![false,false,false]];
+
+        let mut deadboard = vec![vec![false,false,false],
+                                 vec![false,false,false],
+                                 vec![false,false,false]];
+        let mut game = Board::custom_instantiation(twoboard);
+        game = game.next_board_state();
+        assert_eq!(game.board, deadboard);                    
+    }
+
+    #[test]
+    pub fn test_next_on_three_alive() {
+        let mut threeboard =  vec![vec![false,true,false],
+                                 vec![false,true,true],
+                                 vec![false,false,false]];
+
+        let mut nextboard = vec![vec![false,true,true],
+                                 vec![false,true,true],
+                                 vec![false,false,false]];
+        let mut game = Board::custom_instantiation(threeboard);
+        game = game.next_board_state();
+        assert_eq!(game.board, nextboard);                    
+    }
+
+    #[test]
+    pub fn test_pattern_rotated_square() {
+        let mut squareboard =  vec![vec![false,true,false],
+                                 vec![true,true,true],
+                                 vec![false,true,false]];
+
+        let mut nextboard = vec![vec![true,true,true],
+                                 vec![true,false,true],
+                                 vec![true,true,true]];
+        let mut game = Board::custom_instantiation(squareboard);
+        game = game.next_board_state();
+        assert_eq!(game.board, nextboard);                    
+    }
+
+    #[test]
+    pub fn test_pattern_line() {
+        let mut lineboard =  vec![vec![false,true,false],
+                                 vec![false,true,false],
+                                 vec![false,true,false]];
+
+        let mut nextboard = vec![vec![false,false,false],
+                                 vec![true,true,true],
+                                 vec![false,false,false]];
+        let mut game = Board::custom_instantiation(lineboard);
+        game = game.next_board_state();
+        assert_eq!(game.board, nextboard);                    
+    }
+
 }
--- GameOfLife/src/main.rs
+++ GameOfLife/src/main.rs
@@ -1,7 +1,37 @@
 use crate::board::Board;
 mod board;
+use std::{thread, time};
 
 fn main() {
-    let x = Board::random_state_instantiation(3,3);
-    println!("{}", x);
+    let mut glidergun =  vec![vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,true,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,false,false,false,false,false,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,true,true,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,true,false,false,false,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,true,true,false],
+                              vec![false,true,true,false,false,false,false,false,false,false,false,true,false,false,false,false,false,true,false,false,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,true,true,false,false,false,false,false,false,false,false,true,false,false,false,true,false,true,true,false,false,false,false,true,false,true,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,true,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+                              vec![false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]];
+    //let mut x = Board::random_state_instantiation(70,60);
+    let mut x = Board::custom_instantiation(glidergun);
+    let hundred_millis = time::Duration::from_millis(100);
+    while(true){
+        println!("{}", x);
+        x = x.next_board_state();
+        thread::sleep(hundred_millis);
+    }
 }
